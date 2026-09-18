c27df3ad53c192ee1ccf0f10824de28cf618f26c
arnavmaharishi-ctrl <arnav.maharishi@gmail.com>
2026-09-19 01:52:37 +0530

    Day 41 solutions

---
+++ Day41/Q81.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    char str[1000];
+    int count = 0;
+
+    printf("Enter a string: ");
+    scanf(" %[^\n]", str);
+
+    while (str[count] != '\0') {
+        count++;
+    }
+
+    printf("Total characters: %d\n", count);
+
+    return 0;
+}
+
